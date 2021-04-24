aa9b7c3b191b8c590f7146aa4d438bc6225c5c3a
sebbevikstrom1989 <sebastian@vikstroms.se>
2021-04-24 23:47:04 +0200

    Fixed UPDATE functions

--- blog-client-template/admin/js/manage-posts.js
+++ blog-client-template/admin/js/manage-posts.js
@@ -1,64 +1,58 @@
 window.onload = function () {
-    fetchAllPosts();
-  };
-  
-  async function fetchAllPosts() {
-    try {
-      const response = await fetch("http://localhost:5000/posts");
-      const posts = await response.json();
-      console.log(posts);
-  
-      let postHTML = "";
-      for (post of posts) {
-        let dateObj = new Date(post.date);
+  fetchAllPosts();
+};
 
-        console.log(post.author)
-      
-  
-        postHTML += `
+async function fetchAllPosts() {
+  try {
+    const response = await fetch("http://localhost:5000/posts");
+    const posts = await response.json();
+    console.log(posts);
+
+    let postHTML = "";
+    for (post of posts) {
+      let dateObj = new Date(post.date);
+
+      console.log(post.author);
+
+      postHTML += `
         <tr>
             <td>${post.title}</td>
             <td>${post.author}</td>
             <td>${formatDate(dateObj)}</td>
             <td>
-            <a href="#" id="delete-btn" data-id="${
-                post["_id"]}">Delete</a>
-            <a href="/web_com/blog-client-template/admin/create-post.html">Update</a>
+            <a href="#" id="delete-btn" data-id="${post["_id"]}">Delete</a>
+            <a href="/web_com/blog-client-template/admin/update-post.html?id=${
+              post["_id"]
+            }">Update</a>
         </td>
     </tr>
         `;
-  
-        document.querySelector("#blog-content").innerHTML = postHTML;
-      }
 
-      let delBtns = document.querySelectorAll("#delete-btn");
-
-      for(let del of delBtns ){
-        del.addEventListener("click", async function(e) {
-          let delClick = e.target;
-          let postId = delClick.dataset.id;
-
-          try{
-            await fetch("http://localhost:5000/posts/" + postId,{method: "DELETE"})
-            delClick.parentNode.parentNode.remove()
-
-          }catch(error){
-            console.log(error)
-          }
-         
-        })
-        
-      }  
-    } catch (error) {
-      console.log(error);
+      document.querySelector("#blog-content").innerHTML = postHTML;
     }
 
+    let delBtns = document.querySelectorAll("#delete-btn");
 
- 
-  }
-  
-  function formatDate(dateObj) {
-    return `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()} ${dateObj.getHours()}:${dateObj.getMinutes()}`;
-  }
+    for (let del of delBtns) {
+      del.addEventListener("click", async function (e) {
+        let delClick = e.target;
+        let postId = delClick.dataset.id;
 
-  
+        try {
+          await fetch("http://localhost:5000/posts/" + postId, {
+            method: "DELETE",
+          });
+          delClick.parentNode.parentNode.remove();
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function formatDate(dateObj) {
+  return `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDate()} ${dateObj.getHours()}:${dateObj.getMinutes()}`;
+}
